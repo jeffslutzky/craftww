d188f9422abaaae96acb5f1ed04d0277771a9352
Jeff Slutzky <jeffslutzky@gmail.com>
2017-05-24 19:05:39 -0400

    styling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,27 +11,13 @@
       $http.get(vm.url)
         .then(function(response) {
           vm.feed = response.data.data.children;
-          // angular.forEach(vm.feed, function(item) {
-          //   item.checked = false;
-          // });
-          console.log(vm.feed);
         })
         .catch(function (err) {
           console.log('Error: ', err);
         });
     };
 
-    vm.toggle = function(item) {
-      // item["favorite"] = !item["favorite"];
-      console.log("favorited: " + item["favorite"]);
-    }
-
     vm.getFeed();
-
-    vm.toggleFavorites = function() {
-      // vm.showFavorites = !vm.showFavorites;
-      console.log("vm.showFavorites = " + vm.showFavorites);
-    };
 
   }
 
